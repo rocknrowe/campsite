212d6af4f03fd49e26fa18c19224d7b8ac0d3436
Rowena Dsouza <rocknrowe@gmail.com>
2020-02-29 14:22:05 -0500

    First commit - project structure

---
+++ src/main/java/com/example/upgrade/campsite/api/v1/controllers/AvailabilitiesController.java
@@ -0,0 +1,4 @@
+package com.example.upgrade.campsite.api.v1.controllers;
+
+public class AvailabilitiesController {
+}
---
+++ src/test/java/com/example/upgrade/campsite/CampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upgrade.campsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
